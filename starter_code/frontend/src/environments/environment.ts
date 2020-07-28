770414c132c5d712b20d6e489c8702fdffb46661
ksrp410 <kheteshs@gmail.com>
2020-07-28 11:09:34 +0530

    second commit

--- starter_code/frontend/src/environments/environment.ts
+++ starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'ksrp', // the auth0 domain prefix
+    url: 'ksrp.us', // the auth0 domain prefix
     audience: 'Coffee Shop Full Stack', // the audience set for the auth0 app
     clientId: 'zikBtWHm4f15AHme1OlkD8JBis9IcR5j', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
